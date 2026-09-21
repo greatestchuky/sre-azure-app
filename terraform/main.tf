2e33a2212a949463063e37e9f2dc61aab565e3d2
Chuky <ibeanuchuky@gmail.com>
2026-09-22 00:44:01 +0100

    Enforce HTTPS on web app

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_linux_web_app" "app" {
   resource_group_name = azurerm_resource_group.app.name
   location            = azurerm_service_plan.app.location
   service_plan_id     = azurerm_service_plan.app.id
-
+  https_only = true
   site_config {
     always_on = false
   }
